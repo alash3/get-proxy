1dc2a594587fae7a4baac6d05a8ac7e45539c8d3
Albert Lash <alash3@bloomberg.net>
2015-10-09 13:16:39 -0400

    Using npm_config_* environment variables instead of sourcing npmrc

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var conf = require('rc')('npm');
 
 module.exports = function () {
 	return process.env.HTTPS_PROXY ||
 		process.env.https_proxy ||
 		process.env.HTTP_PROXY ||
 		process.env.http_proxy ||
-		conf['https-proxy'] ||
-		conf['http-proxy'] ||
-		conf.proxy ||
+		process.env.ALL_PROXY ||
+		process.env.all_proxy ||
+		process.env.npm_config_https_proxy ||
+		process.env.npm_config_proxy ||
 		null;
 };
